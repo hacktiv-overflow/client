775d8cc053b55ac1a3c7449e886ea0c048681df6
yulianhp <kono.utsukushi24@gmail.com>
2018-03-01 08:27:52 +0700

    masih milih desain

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
     <header>
-      <span>Vue.js PWA</span>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
     </main>
   </div>
@@ -22,33 +20,33 @@ body {
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #2c3e50;*/
 }
 
 main {
-  text-align: center;
-  margin-top: 40px;
+  /*text-align: center;
+  margin-top: 40px;*/
 }
 
 header {
-  margin: 0;
+  /*margin: 0;
   height: 56px;
   padding: 0 16px 0 24px;
   background-color: #35495E;
-  color: #ffffff;
+  color: #ffffff;*/
 }
 
 header span {
-  display: block;
+  /*display: block;
   position: relative;
   font-size: 20px;
   line-height: 1;
   letter-spacing: .02em;
   font-weight: 400;
   box-sizing: border-box;
-  padding-top: 16px;
+  padding-top: 16px;*/
 }
 </style>
---
+++ src/views/LoginRegister.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="container">
+    <div class="card text-white bg-primary mb-3" style="width:80%;margin:auto;margin-top:40%;">
+    <div class="card-header">Header</div>
+    <div class="card-body">
+      <h4 class="card-title">Primary card title</h4>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
